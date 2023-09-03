8dd85c9590a16e1365fe8ac702179f1b9e83ddc6
conneroisu <conneroisu@outlook.com>
2023-09-02 22:49:52 -0500

    fix nui lazy issue

--- lua/custom/plugins/nui-nvim-plugin.lua
+++ lua/custom/plugins/nui-nvim-plugin.lua
@@ -3,8 +3,4 @@
    desc: Neovim UI Library
    author: MunifTanjim (https://github.com/MunifTanjim)
    url: https://github.com/MunifTanjim/nui.nvim
---]===]
-return {
-	"MunifTanjim/nui.nvim",
-   lazy = true
-}
+--]===] return {"MunifTanjim/nui.nvim"}
