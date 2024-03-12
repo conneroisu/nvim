0d4169de470b7a57285f7b09bf4a959d2eb704a9
connero <88785126+conneroisu@users.noreply.github.com>
2024-03-11 19:09:20 -0500

    latest

--- init.lua
+++ init.lua
@@ -241,6 +241,7 @@ lspconfig.pyright.setup {
 	on_attach = on_attach,
 	capabilities = capabilities,
 }
+
 lspconfig.jedi_language_server.setup {
 	on_attach = on_attach,
 	capabilities = capabilities,
