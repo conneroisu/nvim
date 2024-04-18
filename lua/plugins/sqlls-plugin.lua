6dfe45c954bde31b0617574ae83de9072036b99e
connero <88785126+conneroisu@users.noreply.github.com>
2024-04-18 07:12:36 -0500

    latest

--- lua/plugins/sqlls-plugin.lua
+++ lua/plugins/sqlls-plugin.lua
@@ -1,11 +1,5 @@
 return {
-    'nanotee/sqls.nvim' ,
-    config = function ()
-require('lspconfig').sqls.setup{
-    on_attach = function(client, bufnr)
-        require('sqls').on_attach(client, bufnr)
-    end
-}
-        
+    'nanotee/sqls.nvim',
+    config = function()
     end
 }
