9a222425985ba8f028f27c30d1e8ec62ecbf3801
Conner Ohnesorge <connerohnesorge@Conners-MacBook-Air.local>
2024-07-12 15:30:27 -0500

    adedd ale plugin

---
+++ lua/plugins/ale-plugin.lua
@@ -0,0 +1,18 @@
+return {
+	'dense-analysis/ale',
+	dependencies = {
+		'bufbuild/vim-buf',
+	},
+	config = function()
+		-- Configuration goes here.
+		local g = vim.g
+
+		g.ale_ruby_rubocop_auto_correct_all = 1
+
+		g.ale_linters = {
+			ruby = { 'rubocop', 'ruby' },
+			lua = { 'lua_language_server' },
+			proto = { 'buf-lint' },
+		}
+	end
+}
