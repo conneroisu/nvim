6884a44a06eb49831bdc2cb77d504d77b343ac3d
connerohnesorge <conneroisu@outlook.com>
2024-12-21 10:42:49 -0600

    remove vim-dadbod adn update obsidian and doppler

--- lua/plugins/doppler-plugin.lua
+++ lua/plugins/doppler-plugin.lua
@@ -1,9 +1,12 @@
 return {
   "conneroisu/doppler.nvim",
-  dir = "~/Documents/001Repos/doppler.nvim",
+  -- dir = "~/Documents/001Repos/doppler.nvim",
   event = "VeryLazy",
   opts = {
     scope = "~/dotfiles/.config/nvim",
+    variables = {
+      openai_api_key = "OPENAI_API_KEY",
+    },
   },
   dependencies = {
     'nvim-lua/plenary.nvim',
