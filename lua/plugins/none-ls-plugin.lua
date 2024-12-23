77b449a1fe25a13158327622e201bef682ed72f3
connerohnesorge <conneroisu@outlook.com>
2024-12-23 12:45:23 -0600

    latest

---
+++ lua/plugins/none-ls-plugin.lua
@@ -0,0 +1,15 @@
+return {
+  "nvimtools/none-ls.nvim",
+  config = function()
+    local null_ls = require("null-ls")
+
+    null_ls.setup({
+      sources = {
+        null_ls.builtins.diagnostics.revive,
+        null_ls.builtins.diagnostics.golangci_lint,
+        null_ls.builtins.diagnostics.statix,
+        null_ls.builtins.code_actions.statix,
+      },
+    })
+  end,
+}
