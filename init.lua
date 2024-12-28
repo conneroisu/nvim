2c757c31696f2935a7393358f45cefe45d8c5d0a
connerohnesorge <conneroisu@outlook.com>
2024-12-28 14:51:12 -0600

    move format user command from init.lua to lspconfig-plugin.lua

--- init.lua
+++ init.lua
@@ -82,11 +82,6 @@ vim.filetype.add {
   },
 }
 
--- Format :Format command
-vim.api.nvim_create_user_command("Format", function()
-  vim.lsp.buf.format()
-end, {})
-
 vim.api.nvim_set_hl(0, "CmpGhostText", { link = "Comment", default = true })
 
 vim.o.number = true
--- lua/plugins/lspconfig-plugin.lua
+++ lua/plugins/lspconfig-plugin.lua
@@ -155,5 +155,10 @@ return {
         end
       end,
     })
+
+    -- Format :Format command
+    vim.api.nvim_create_user_command("Format", function()
+      vim.lsp.buf.format()
+    end, {})
   end,
 }
