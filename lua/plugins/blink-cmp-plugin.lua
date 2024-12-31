b6ab89d478a1bbf7c2b9dcd9a57ef641a044a9b3
connerohnesorge <conneroisu@outlook.com>
2024-12-31 08:39:56 -0600

    added snippets and removed luasnip

--- lua/plugins/blink-cmp-plugin.lua
+++ lua/plugins/blink-cmp-plugin.lua
@@ -1,27 +1,16 @@
 return {
   "saghen/blink.cmp",
   version = 'v0.7.6',
-  dependencies = {
-    -- "L3MON4D3/LuaSnip",
-
-  },
   opts = {
-    -- keymap = { preset = 'enter' },
-    snippets = {
-      -- expand = function(snippet) require('luasnip').lsp_expand(snippet) end,
-      -- active = function(filter)
-      --   if filter and filter.direction then
-      --     return require('luasnip').jumpable(filter.direction)
-      --   end
-      --   return require('luasnip').in_snippet()
-      -- end,
-      -- jump = function(direction) require('luasnip').jump(direction) end,
-    },
+    debug = true,
     sources = {
       default = { "lsp", "path", "snippets", "buffer", "lazydev" },
       providers = {
         -- dont show LuaLS require statements when lazydev has items
-        lsp = { fallback_for = { "lazydev" } },
+        lsp = {
+          fallback_for = { "lazydev" },
+          score_offset = 2,
+        },
         lazydev = { name = "LazyDev", module = "lazydev.integrations.blink", },
       },
     },
