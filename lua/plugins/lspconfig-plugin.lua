63c9509b11c2de680a57d8a138ce02012254727f
connerohnesorge <conneroisu@outlook.com>
2025-02-11 09:14:53 -0600

    latest

--- lua/plugins/lspconfig-plugin.lua
+++ lua/plugins/lspconfig-plugin.lua
@@ -52,29 +52,23 @@ return {
       basedpyright = {},
       protols = {},
       ts_ls = {},
-      -- golangci_lint_ls = {},
       texlab = {},
       tailwindcss = {},
       jsonls = {},
       yamlls = {},
-      -- hdl_checker = {},
       dockerls = {},
       astro = {},
       svelte = {},
-      -- nushell = {},
       html = {},
       htmx = {},
       hyprls = {},
       cssls = {},
-      -- nil_ls = {},
       nixd = {
         formatting = {
           format_on_save = true,
         },
       },
-      ---@module "lspconfig.configs.gopls"
       gopls = {
-
         settings = {
           gopls = {
             experimentalPostfixCompletions = true,
@@ -89,7 +83,6 @@ return {
           usePlaceholders = true,
         }
       },
-      -- ocamlls = {},
       zls = {},
       templ = {},
       marksman = {},
@@ -115,16 +108,6 @@ return {
         ["<C-f>"] = cmp.mapping.scroll_docs(4),
         ["<C-Space>"] = cmp.mapping.complete(),
         ["<C-e>"] = cmp.mapping.close(),
-        ["<Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_next_item()
-          else
-            fallback()
-          end
-        end, {
-          "i",
-          "s",
-        }),
         ["<S-Tab>"] = cmp.mapping(function(fallback)
           if cmp.visible() then
             cmp.select_prev_item()
@@ -143,7 +126,7 @@ return {
         { name = "nvim_lsp_signature_help" },
         { name = "buffer" },
         { name = "path" },
-        { name = "cmdline" },
+        -- { name = "cmdline" },
       },
       formatting = {
         format = function(entry, vim_item)
@@ -157,9 +140,13 @@ return {
           return vim_item
         end,
       },
-      confirmation = { completeopt = 'menu,menuone,noinsert' },
+      completion = {
+        completeopt = 'menu,menuone,noinsert'
+      }
     })
-    require('lspsaga').setup({})
+    require('lspsaga').setup({
+      lightbulb = { enable = false },
+    })
     local capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
     vim.filetype.add({ extension = { templ = "templ" } })
@@ -226,35 +213,78 @@ return {
             end,
           })
         end
+        -- Key Mappings
         if client.supports_method('textDocument/rename') then
-          vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, { buffer = args.buf, desc = '[R]e[n]ame' })
+          vim.keymap.set(
+            'n',
+            '<leader>rn',
+            vim.lsp.buf.rename,
+            { buffer = args.buf, desc = '[R]e[n]ame' }
+          )
         end
         if client.supports_method('textDocument/formatting') then
-          vim.keymap.set('n', '<leader>cf', vim.lsp.buf.format, { buffer = args.buf, desc = '[C]ode [F]ormat' })
+          vim.keymap.set(
+            'n',
+            '<leader>cf',
+            vim.lsp.buf.format,
+            { buffer = args.buf, desc = '[C]ode [F]ormat' }
+          )
         end
         if client.supports_method('textDocument/codeAction') then
-          vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, { buffer = args.buf, desc = '[C]ode [A]ction' })
+          vim.keymap.set(
+            'n',
+            '<leader>ca',
+            vim.lsp.buf.code_action,
+            { buffer = args.buf, desc = '[C]ode [A]ction' }
+          )
         end
         if client.supports_method('textDocument/documentSymbol') then
-          vim.keymap.set('n', '<leader>ls', vim.lsp.buf.document_symbol,
-            { buffer = args.buf, desc = '[L]ocate [S]ymbols' })
+          vim.keymap.set(
+            'n',
+            '<leader>ls',
+            vim.lsp.buf.document_symbol,
+            { buffer = args.buf, desc = '[L]ocate [S]ymbols' }
+          )
         end
         if client.supports_method('workspace/symbol') then
-          vim.keymap.set('n', '<leader>lws', vim.lsp.buf.workspace_symbol,
-            { buffer = args.buf, desc = '[W]orkspace [S]ymbols' })
+          vim.keymap.set(
+            'n',
+            '<leader>lws',
+            vim.lsp.buf.workspace_symbol,
+            { buffer = args.buf, desc = '[W]orkspace [S]ymbols' }
+          )
         end
         if client.supports_method('textDocument/signatureHelp') then
-          vim.keymap.set('n', '<leader>sh', vim.lsp.buf.signature_help,
-            { buffer = args.buf, desc = '[S]ignature [H]elp' })
+          vim.keymap.set(
+            'n',
+            '<leader>sh',
+            vim.lsp.buf.signature_help,
+            { buffer = args.buf, desc = '[S]ignature [H]elp' }
+          )
         end
         if client.supports_method('textDocument/hover') then
-          vim.keymap.set('n', 'K', vim.lsp.buf.hover, { buffer = args.buf, desc = '[H]over' })
+          vim.keymap.set(
+            'n',
+            'K',
+            vim.lsp.buf.hover,
+            { buffer = args.buf, desc = '[H]over' }
+          )
         end
         if client.supports_method('textDocument/definition') then
-          vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { buffer = args.buf, desc = '[D]efinition' })
+          vim.keymap.set(
+            'n',
+            'gd',
+            vim.lsp.buf.definition,
+            { buffer = args.buf, desc = '[D]efinition' }
+          )
         end
         if client.supports_method('textDocument/references') then
-          vim.keymap.set('n', '<leader>lr', vim.lsp.buf.references, { buffer = args.buf, desc = '[R]eferences' })
+          vim.keymap.set(
+            'n',
+            '<leader>lr',
+            vim.lsp.buf.references,
+            { buffer = args.buf, desc = '[R]eferences' }
+          )
         end
         if client.supports_method('textDocument/implementation') then
           vim.keymap.set('n', '<leader>li', vim.lsp.buf.implementation, { buffer = args.buf, desc = '[I]mplementation' })
@@ -265,7 +295,7 @@ return {
       end,
     })
 
-    -- Format :Format command
+    -- :Format command
     vim.api.nvim_create_user_command("Format", function()
       vim.lsp.buf.format()
     end, {})
