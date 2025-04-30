f6a702b0b8981c855d3eeb2b9bd2256530ebc100
connerohnesorge <conneroisu@outlook.com>
2025-04-29 19:57:15 -0500

    fully removed cmp from lspconfig

--- lua/plugins/dap-plugin.lua
+++ lua/plugins/dap-plugin.lua
@@ -1,5 +1,7 @@
 return {
   "mfussenegger/nvim-dap",
+
+  event = "VeryLazy",
   dependencies = {
     "rcarriga/nvim-dap-ui",
     "mfussenegger/nvim-dap",
--- lua/plugins/lspconfig-plugin.lua
+++ lua/plugins/lspconfig-plugin.lua
@@ -9,19 +9,10 @@ return {
   dependencies = {
     'j-hui/fidget.nvim',
     "folke/lazydev.nvim",
-    -- "saghen/blink.cmp",
     "folke/neoconf.nvim",
-    -- 'hrsh7th/cmp-nvim-lsp',
-    -- "hrsh7th/cmp-nvim-lsp-signature-help",
-    -- 'hrsh7th/cmp-buffer',
-    -- 'hrsh7th/cmp-path',
-    -- 'hrsh7th/cmp-cmdline',
-    -- 'hrsh7th/nvim-cmp',
-    -- 'hrsh7th/cmp-vsnip',
     'hrsh7th/vim-vsnip',
     'nvim-treesitter/nvim-treesitter',
     'nvim-tree/nvim-web-devicons',
-    -- 'nvimdev/lspsaga.nvim',
   },
   opts = {
     diagnostics = {
@@ -147,61 +138,6 @@ return {
     }
   },
   config = function(_, opts)
-    -- local cmp = require("cmp")
-    -- cmp.setup({
-    --   snippet = {
-    --     expand = function(args)
-    --       vim.fn["vsnip#anonymous"](args.body)
-    --     end,
-    --   },
-    --   mapping = {
-    --     ["<C-p>"] = cmp.mapping.select_prev_item(),
-    --     ["<C-n>"] = cmp.mapping.select_next_item(),
-    --     ["<C-y>"] = cmp.mapping.confirm({
-    --       behavior = cmp.ConfirmBehavior.Insert,
-    --       select = true,
-    --     }),
-    --     ["<C-d>"] = cmp.mapping.scroll_docs(-4),
-    --     ["<C-f>"] = cmp.mapping.scroll_docs(4),
-    --     ["<C-Space>"] = cmp.mapping.complete(),
-    --     ["<C-e>"] = cmp.mapping.close(),
-    --     ["<S-Tab>"] = cmp.mapping(function(fallback)
-    --       if cmp.visible() then
-    --         cmp.select_prev_item()
-    --       elseif require("luasnip").jumpable(-1) then
-    --         vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-jump-prev", true, true, true), "")
-    --       else
-    --         fallback()
-    --       end
-    --     end, {
-    --       "i",
-    --       "s",
-    --     }),
-    --   },
-    --   sources = {
-    --     { name = "nvim_lsp" },
-    --     { name = "nvim_lsp_signature_help" },
-    --     { name = "buffer" },
-    --     { name = "path" },
-    --     -- { name = "cmdline" },
-    --   },
-    --   formatting = {
-    --     format = function(entry, vim_item)
-    --       vim_item.kind = string.format("%s", vim_item.kind)
-    --       vim_item.menu = ({
-    --         nvim_lsp = "[LSP]",
-    --         buffer = "[BUF]",
-    --         path = "[PATH]",
-    --         cmdline = "[CMD]",
-    --       })[entry.source.name]
-    --       return vim_item
-    --     end,
-    --   },
-    --   completion = {
-    --     completeopt = 'menu,menuone,noinsert'
-    --   }
-    -- })
-
     vim.filetype.add({ extension = { templ = "templ" } })
     local lspconfig = require("lspconfig")
     for server, config in pairs(opts.servers) do
@@ -307,12 +243,12 @@ return {
             desc = '[I]mplementation'
           })
         vim.keymap.set('n', '<leader>lt', vim.lsp.buf.type_definition, { buffer = args.buf, desc = '[T]ypeDefinition' })
+
+        -- :Format command
+        vim.api.nvim_create_user_command("Format", function()
+          vim.lsp.buf.format({ bufnr = args.buf, async = true })
+        end, {})
       end,
     })
-
-    -- :Format command
-    vim.api.nvim_create_user_command("Format", function()
-      vim.lsp.buf.format()
-    end, {})
   end,
 }
